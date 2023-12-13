f3695e02dc07a1d239186057e3a63ead04a7e22d
alfred0496 <alfredmakura6@gmail.com>
2023-12-13 14:38:27 -0800

    makk

---
+++ shell.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "shell.h"
+
+/**
+ * main - entry point
+ * Return: Always 0
+ * printf("Hello, Betty!\n");
+ * return (0);
+*/
